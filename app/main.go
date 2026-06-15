226bc62b00affd571d99dcbea967179d7570e4b8
Eliel Delfino <eliel.delfino@fanut.ufal.br>
2026-06-15 20:52:49 -0300

    updating app with prometheus imports and /metrics

--- app/main.go
+++ app/main.go
@@ -5,6 +5,17 @@ import (
 	"encoding/json"
 	"log"
 	"time"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var httpRequestsTotal = promauto.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Total number of HTTP requests",
+	},
+	[]string{"method", "code"},
 )
 
 type Response struct {
@@ -12,7 +23,7 @@ type Response struct {
 	Horario   string `json:"horario"`
 }
 
-func projetoHandler(w http.ResponseWriter, r *http.Request) {
+func projectHandler(w http.ResponseWriter, r *http.Request) {
 	response := Response{
 		Nome:    "Projeto Korp",
 		Horario: time.Now().UTC().Format("15:04:05 UTC"),
@@ -27,7 +38,12 @@ func projetoHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /projeto-korp", projetoHandler)
+	mux.Handle("GET /projeto-korp",
+		promhttp.InstrumentHandlerCounter(httpRequestsTotal, http.HandlerFunc(projectHandler),
+		),
+	)
+
+	mux.Handle("GET /metrics", promhttp.Handler())
 
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
